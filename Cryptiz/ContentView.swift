c6003c78015d7d8e5e4978dada8e029644ded96e
Teddy Juhlin-Henricson <teddy.juhlinhenricson@daresay.co>
2023-10-31 15:18:57 +0100

    Init git with .gitignore

---
+++ Cryptiz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cryptiz
+//
+//  Created by Teddy Juhlin-Henricson on 2023-10-31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Cryptiz!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Cryptiz/CryptizApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptizApp.swift
+//  Cryptiz
+//
+//  Created by Teddy Juhlin-Henricson on 2023-10-31.
+//
+
+import SwiftUI
+
+@main
+struct CryptizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
